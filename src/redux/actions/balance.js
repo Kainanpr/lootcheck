58dd297ec62d29e50180039dc2ae0a43ff17a987
Kainan Ramos <Kainan.Ramos@vistajet.com>
2019-05-08 10:28:12 -0300

    [new feature|test] - creates an action to set the balance

---
+++ src/redux/actions/balance.js
@@ -0,0 +1,10 @@
+import * as constants from './constants';
+
+export const setBalance = balance => ({
+  type: constants.SET_BALANCE,
+  balance,
+});
+
+export default {
+  setBalance,
+};
---
+++ src/redux/actions/constants.js
@@ -0,0 +1,5 @@
+export const SET_BALANCE = 'SET_BALANCE';
+
+export default {
+  SET_BALANCE,
+};
